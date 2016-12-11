fb2dcb224fd636128bac64efb3ccc48c8bed9e34
tblasig <thomasblasig@sfr.fr>
2016-12-11 15:47:51 +0100

    Initialization of project
    Angular, ES6, ThreeJs, Vizicities

--- scripts/components/content/content.controller.js
+++ scripts/components/content/content.controller.js
@@ -1,6 +1,57 @@
 class ContentController {
     constructor() {
-        this.body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor lacus vulputate lectus congue scelerisque. Duis nulla neque, interdum pulvinar lectus eu, lobortis pretium ante. Suspendisse et justo odio. Sed tincidunt est lacus. Fusce vitae rhoncus est. Duis vulputate vel urna id elementum. In dignissim vitae lectus id fermentum. Cras nec dui nec felis elementum interdum venenatis vel felis. Curabitur nec tristique nisl, ac consequat risus. Morbi vitae diam sed massa hendrerit consequat eu quis velit.';
+
+        // Toulouse
+        this._coords = [43.600000, 1.433333];
+        this.initWorld();
+    }
+
+    initWorld() {
+
+        this._world = VIZI.world('world', {
+            skybox: true,
+            postProcessing: false
+        }).setView(this._coords);
+
+        // Set position of sun in sky
+        this._world._environment._skybox.setInclination(0.1);
+
+        // Add controls
+        VIZI.Controls.orbit().addTo(this._world);
+
+        // CartoDB basemap
+        VIZI.imageTileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+        }).addTo(this._world);
+
+        // Buildings and roads from Mapzen (polygons and linestrings)
+        var topoJSONTileLayer = VIZI.topoJSONTileLayer('https://vector.mapzen.com/osm/buildings,roads/{z}/{x}/{y}.topojson?api_key=vector-tiles-NT5Emiw', {
+            interactive: false,
+            style: function(feature) {
+                var height;
+
+                if (feature.properties.height) {
+                    height = feature.properties.height;
+                } else {
+                    height = 10 + Math.random() * 10;
+                }
+
+                return {
+                    height: height,
+                    lineColor: '#f7c616',
+                    lineWidth: 1,
+                    lineTransparent: true,
+                    lineOpacity: 0.2,
+                    lineBlending: THREE.AdditiveBlending,
+                    lineRenderOrder: 2
+                };
+            },
+            filter: function(feature) {
+                // Don't show points
+                return feature.geometry.type !== 'Point';
+            },
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://whosonfirst.mapzen.com#License">Who\'s On First</a>.'
+        }).addTo(this._world);
     }
 }
 
--- scripts/components/navbar/navbar.controller.js
+++
@@ -1,7 +0,0 @@
-class NavbarController {
-    constructor() {
-        this.name = 'My Cool App';
-    }
-}
-
-export default NavbarController;
